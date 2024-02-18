897dc8ce47fbfbd5d1866e0721fd32ddb1c4b5bb
arish2961ki <153129795+arish2961ki@users.noreply.github.com>
2024-02-18 13:07:21 -0700

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,65 @@
+<?php
+$oc_config = array (
+    
+    //================================================================================================
+   
+
+    "favicon"         => "favicon.png" ,
+
+    //================================================================================================
+
+    "sitelogo"        => "Ultra HD - Movie & TV Show",
+    
+    "sitename"        => "iflixonline",
+
+    "sitedescription" => "Watch Full Movies Streaming HD Quality for FREE",
+
+    "sitekeywords"    => "full movie, full episode",
+
+    "sitethemes"      => "g-movie",
+
+    "offer_link1"     => "//look.udncoeln.com/offer?prod=964&ref=5200540",
+
+    "offer_link2"     => "//look.udncoeln.com/offer?prod=964&ref=5200540",
+
+    // "sport_offer"     => "",        // Jika Belum Di Include Plugin Sport nya gak bisa di gunakan
+
+    "histatsID"       => "4352413",
+
+    //================================================================================================
+
+    "tvdb_search"     => "true",
+
+    "cache"           => "true",
+
+    "_seo"            => "true",
+
+    "_stt"            => "true",
+
+    "protect_content" => "true",       // disable klik kanan, block text, copy paste pada halaman LP
+
+    "search_url"      => "search",
+
+    "url_end"         => ".html",
+    
+    "category_url"    => "genre",
+    
+    "timezone"        => "Asia/Jakarta",
+
+    "debug"           => "true",
+
+);
+
+$alt_country2 = array(
+    /*
+    |--------------------------------------------------------------------------
+    | Contoh Penulisan Country Code
+    |--------------------------------------------------------------------------
+    | 'US', 'GB', 'UK'
+    | Contoh Dibawah Menandakan Negara ID (Indonesia) dan MY (Malaysia) 
+    | Offer Link nya akan di redirect ke Offer Kedua
+    |
+    */
+   
+   'ID', 'MY', 'MX', 'RU'
+);
